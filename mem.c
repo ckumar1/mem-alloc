67273475e6fd19e3839025d6b4c82c76cce81c3a
Cherub Kumar <cherub.kumar@gmail.com>
2014-04-02 17:36:58 -0500

    Initialize Project
    created project files

---
+++ mem.c
@@ -0,0 +1,22 @@
+#include "mem.h"
+
+int Mem_Init(int sizeOfRegion) 
+{
+
+	return (0);
+}
+
+void *Mem_Alloc(int size)
+{
+
+	return (0);
+}
+
+int Mem_Free(void *ptr) {
+
+	return (0);
+}
+
+void Mem_Dump() {
+
+}
