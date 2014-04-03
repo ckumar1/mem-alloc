3d4c7a22b77d99bde7a76ef5f0aba338a28a8c20
Cherub Kumar <cherub.kumar@gmail.com>
2014-04-02 19:59:26 -0500

    add error checking for mem_init

--- mem.c
+++ mem.c
@@ -1,8 +1,29 @@
 #include "mem.h"
+#include <stdio.h>
+#include <unistd.h>
+
+
+int m_error;
+
+static int m_init_flag = 0;
 
 int Mem_Init(int sizeOfRegion) 
 {
 
+	// check for errors
+	if ( m_init_flag != 0 || sizeOfRegion <= 0)
+	{
+		// set m_error for bad args
+		m_error = E_BAD_ARGS;
+		return (-1);
+	}
+
+	// set init flag to prevent Mem_Init from being run again
+	m_init_flag = 1;
+
+
+
+	// return 0 on success
 	return (0);
 }
 
--- mem_test.c
+++ mem_test.c
@@ -1,6 +1,8 @@
 #include "mem.h"
+#include <stdio.h>
+#include <unistd.h>
 
 int main() {
-	
+	printf( "%i\n", getpagesize() );
 	return (0);
 }
