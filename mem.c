02d9fae04a30f45a0cd394bbbc9e5d61b7bd318f
Cherub Kumar <cherub.kumar@gmail.com>
2014-04-05 01:48:11 -0500

    Add test code and string messages in Mem_Init

--- mem.c
+++ mem.c
@@ -9,10 +9,13 @@ static int m_init_flag = 0;
 
 int Mem_Init(int sizeOfRegion) 
 {
+	puts("Mem_Init starts...\n");
+	printf("Size of region requested: %d\n", sizeOfRegion);
 
 	// check for errors
 	if ( m_init_flag != 0 || sizeOfRegion <= 0)
 	{
+		puts("Bad arguments for Mem_Init!\n\nEither already been run once or sizeofRegion was not above zero");
 		// set m_error for bad args
 		m_error = E_BAD_ARGS;
 		return (-1);
@@ -20,8 +23,8 @@ int Mem_Init(int sizeOfRegion)
 
 	// set init flag to prevent Mem_Init from being run again
 	m_init_flag = 1;
-
-
+	
+	puts("Mem_Init Ending.");
 
 	// return 0 on success
 	return (0);
