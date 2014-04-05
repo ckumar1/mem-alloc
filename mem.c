8704c978968b393e44b3e5d68aae8233f501f0bd
Cherub Kumar <cherub.kumar@gmail.com>
2014-04-05 02:11:38 -0500

    [Mem_Init] Implement round-up functionality for requested size of region

--- mem.c
+++ mem.c
@@ -10,12 +10,12 @@ static int m_init_flag = 0;
 int Mem_Init(int sizeOfRegion) 
 {
 	puts("Mem_Init starts...\n");
-	printf("Size of region requested: %d\n", sizeOfRegion);
+	printf("Requested Size: %d\n", sizeOfRegion);
 
 	// check for errors
 	if ( m_init_flag != 0 || sizeOfRegion <= 0)
 	{
-		puts("Bad arguments for Mem_Init!\n\nEither already been run once or sizeofRegion was not above zero");
+		puts("Bad arguments for Mem_Init!\n\nEither already been run once or sizeOfRegion was not above zero");
 		// set m_error for bad args
 		m_error = E_BAD_ARGS;
 		return (-1);
@@ -23,6 +23,17 @@ int Mem_Init(int sizeOfRegion)
 
 	// set init flag to prevent Mem_Init from being run again
 	m_init_flag = 1;
+
+	
+	// Round up the requested size of region to the nearest page size
+	int pageSize = getpagesize();
+	int partialSpace = sizeOfRegion % pageSize;
+	int addedSpace = pageSize  - partialSpace;
+	int roundedSize = sizeOfRegion + addedSpace;
+
+	printf("Rounded Size: %d\n", roundedSize );	
+	
+	
 	
 	puts("Mem_Init Ending.");
 
