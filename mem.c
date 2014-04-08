460eac0dc6bff589543a8875a63b751eac934e14
Cherub Kumar <cherub.kumar@gmail.com>
2014-04-08 17:40:16 -0500

    Start Top down implementation of [mem_alloc] -Implemented mem_alloc and bestfitFor -Started bestfitChunk... (still need search and header setting) -refactor align function to use size_t type -general refactoring and cleanup

--- mem.c
+++ mem.c
@@ -6,7 +6,6 @@
 #include <sys/mman.h>
 #include <stdbool.h>
 
-
 /* Global Variables */
 
 int m_error;
@@ -21,41 +20,67 @@ typedef struct node_t {
 
 // Header for an allocated block
 typedef struct header_t {
-	int size; 
+	int size;
 } header_t;
 
 node_t* head; // pointer to the head of the free list
-
 
 /* Helper Functions */
 
 // TODO maybe needs to be fixed so 4096 doesn't allocate a new page?
 //  Should have enough space for our data structures on top of requested data
-int align(size_t size, unsigned int alignment ) {
+size_t align(size_t size, size_t alignment) {
 	// Round up the requested size to the next highest multiple of alignment
 
 	// Note: if partialSpace==0, function still rounds up to the next page by adding 4096
-    int partialSpace = size % alignment;
-	int addedSpace = alignment - partialSpace;
+	size_t partialSpace = size % alignment;
+	size_t addedSpace = alignment - partialSpace;
 
-	int alignedSize = size + addedSpace;
+	size_t alignedSize = size + addedSpace;
 	return alignedSize;
 }
 
-int Mem_Init(int sizeOfRegion) 
-{
 
+/* bestfitChunk(size_t size)
+ *
+ * Searches for a free chunk in the free list that can fit size
+ * returns address to requested space on success
+ * returns NULL on failure
+ */
+void * bestfitChunk(size_t size) {
+	void * ptr = NULL;
+
+	// TODO Search for free space
+	// TODO if found, set header and *ptr; else return NULL
+
+	return ptr;
+}
+
+/*
+ *  Check if there is enough free space to satisfy requested size
+ * if found, sets header and splits free chunk
+ * On Success, returns void* ptr to the start of the requested spaced
+ * On Failure, returns NULL
+ */
+void * bestfitFor(size_t size) {
+
+	void* ptr = bestfitChunk(size);
+
+	// Set m_error inside bestfitFor
+	if (ptr == NULL) {
+		m_error = E_NO_SPACE;
+	}
+	return (ptr);
+}
+
+
+int Mem_Init(int sizeOfRegion) {
 
 	puts("Mem_Init starts...\n");
 	printf("Requested Size: %d\n", sizeOfRegion);
 
-	// check for errors
-	if ( m_init_flag != 0 || sizeOfRegion <= 0)
-	{
-		// TEST output
-		puts("Bad arguments for Mem_Init!\n\n\
-		Either already been run once or sizeOfRegion was not above zero");
-
+	// check for invalid args and attempts to run multiple times
+	if (m_init_flag != 0 || sizeOfRegion <= 0) {
 		m_error = E_BAD_ARGS;
 		return (-1);
 	}
@@ -64,47 +89,47 @@ int Mem_Init(int sizeOfRegion)
 	m_init_flag = 1;
 
 	// Make sure there is enough memory for the free list
-	size_t heapSize = sizeOfRegion + sizeof (node_t);
-	
+	size_t heapSize = sizeOfRegion + sizeof(node_t);
+
 	printf("Free list Node Size: %u\n", (unsigned int) sizeof(node_t)); // TEST output
 
 	// Align the requested heap size to the nearest page size
 	size_t alignedSize = align(heapSize, getpagesize());
-	printf("Aligned Size: %u\n", (unsigned int) alignedSize ); // TEST output
-	
-
+	printf("Aligned Size: %u\n", (unsigned int) alignedSize); // TEST output
 
 	// open the /dev/zero device
 	int fd = open("/dev/zero", O_RDWR);
-	
+
 	// roundedSize (in bytes) is evenly divisible by the page size
 	head = mmap(NULL, alignedSize, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-	
-	if (head == MAP_FAILED) { perror("mmap"); exit(1); }
+
+	if (head == MAP_FAILED) {
+		perror("mmap");
+		exit(1);
+	}
 	// close the device (don't worry, mapping should be unaffected)
 	close(fd);
 
-
 	// Initialize fields of head free list
-	head->size = alignedSize; // TODO need to take into account size of header
+	head->size = alignedSize;
 	head->next = NULL;
 
 	printf("Free Space: %i\n", head->size); // TEST output
-	
+
 	puts("Mem_Init Ending."); //TEST output
 
 	// return 0 on success
 	return (0);
 }
 
-void *Mem_Alloc(int size)
-{
-	// TODO Align size to 8 byte chunks
-	// TODO Check if bestfit() can find space for requested size
-	// TODO On success, returns void* ptr to the start of the requested space 
-	// 		(&alloc_block + sizeof(header))
-	void* ptr = head; 
-	return (ptr);
+void *Mem_Alloc(int size) {
+
+	// Total allocated size is requestedSize + sizeof(header)
+	size_t allocSize = size + sizeof(header_t);
+	// Align size to 8 byte chunks
+	size_t alignedAllocSize = align(allocSize, 8);
+
+	return bestfitFor(alignedAllocSize);
 }
 
 int Mem_Free(void *ptr) {
